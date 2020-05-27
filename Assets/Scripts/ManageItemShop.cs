093bae000d8b189dad4f21dba8f7b44ef87c48da
hhaven <54193283+hhaven@users.noreply.github.com>
2020-05-26 20:24:42 -0600

    Adedd store
    Unity ver 3.14f

---
+++ Assets/Scripts/ManageItemShop.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageItemShop : MonoBehaviour
+{
+    public GameObject Panel;
+    public GameObject Panel2;
+
+    public void OpenPanel()
+    {
+        
+        if (Panel != null)
+        {
+            Panel2.SetActive(false);
+            Panel.SetActive(true);
+        }
+    }
+}
