68a4e15956945a39bda0db2302372b694a998cce
x-AbsolutZero-x <00170618@uca.edu.sv>
2020-05-28 11:47:16 -0600

    Añadido boton en Characters para retornar a Town.
    Se agrego boton en scene Characters para regresar a scene Town.

---
+++ Assets/Scripts/CharacterPanel/BackButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackButton : MonoBehaviour
+{
+    public void closeButton()
+    {
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Characters"))
+        {
+            SceneManager.LoadScene("Town");
+        }
+    }
+}
