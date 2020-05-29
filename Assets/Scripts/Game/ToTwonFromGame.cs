6fb5716b3323e21951e3166349b51e04c6f81f93
hhaven <54193283+hhaven@users.noreply.github.com>
2020-05-28 18:45:10 -0600

    Collideenemy

---
+++ Assets/Scripts/Game/ToTwonFromGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToTwonFromGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void TownReturn()
+    {
+
+        SceneManager.LoadScene("Town");
+
+    }
+}
