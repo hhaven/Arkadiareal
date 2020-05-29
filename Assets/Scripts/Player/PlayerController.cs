a1d8468c8dd6c1bf668008951040a7ba1ad5883e
hhaven <54193283+hhaven@users.noreply.github.com>
2020-05-28 20:52:01 -0600

    ResolutionFixes

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public Joystick joistick;
     public float moveSpeed;
+    float horizontalMove;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,14 +17,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        if(joistick.Horizontal > 0.5f || joistick.Horizontal < -0.5f)
         {
-            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            transform.Translate(new Vector3(joistick.Horizontal * moveSpeed * Time.deltaTime, 0f, 0f));
         }
 
-        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        if (joistick.Vertical > 0.5f || joistick.Vertical < -0.5f)
         {
-            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            transform.Translate(new Vector3(0f, joistick.Vertical * moveSpeed * Time.deltaTime, 0f));
         }
+
     }
 }
