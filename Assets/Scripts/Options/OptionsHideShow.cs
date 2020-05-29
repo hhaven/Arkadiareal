7885699f5300a61c3bae15b2ed45a28a26bca8c7
hhaven <54193283+hhaven@users.noreply.github.com>
2020-05-28 19:53:33 -0600

    Fixing resolution

---
+++ Assets/Scripts/Options/OptionsHideShow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsHideShow : MonoBehaviour
+{
+    public GameObject Panel;
+    // Start is called before the first frame update
+    public void ButtonReturns()
+    {
+        if (Panel != null)
+        {
+            Panel.SetActive(true);
+        }
+
+
+        
+    }
+}
