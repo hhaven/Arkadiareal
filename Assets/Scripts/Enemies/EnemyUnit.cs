4b8a4c65f7c78f4db8d44d1d29dd1955f87e9d18
hhaven <54193283+hhaven@users.noreply.github.com>
2020-07-01 01:05:43 -0600

    Prefabs, battlestation, enemy and player spawn

---
+++ Assets/Scripts/Enemies/EnemyUnit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyUnit : MonoBehaviour
+{
+    public string unitName;
+    public int unitLevel;
+
+    public int damage;
+
+    public int maxHP;
+    public int currentHP;
+}
