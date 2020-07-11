e34b52e27fc804a1533adc5a44990d9a86449553
RafaMelara98 <00182516@uca.edu.sv>
2020-07-11 02:57:28 -0600

    add Item db

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -8,14 +8,16 @@ public class Item
 	public string decription;
 	public Dictionary<string, int> stats = new Dictionary<string, int>();
 	public int price;
+	public string type;
 
-	public Item(int id, string title, string description,  Dictionary<string, int> stats, int price)
+	public Item(int id, string title, string description,  Dictionary<string, int> stats, int price, string type)
 	{
 		this.id = id;
 		this.title = title;
 		this.decription = decription;
 		this.stats = stats;
 		this.price = price;
+		this.type = type;
 	}
 
 	public Item(Item item)
@@ -25,6 +27,8 @@ public class Item
 		this.decription = item.decription;
 		this.stats = item.stats;
 		this.price = item.price;
+		this.type = item.type;
+
 	}
 
 }
--- Assets/Scripts/Items/ItemDB.cs
+++ Assets/Scripts/Items/ItemDB.cs
@@ -21,6 +21,11 @@ public class ItemDB : MonoBehaviour
         return items.Find(item => item.title == title);
     }
 
+    public ItemDB GetItem(string type)
+    {
+        return items.Find(item => item.type == type);
+    }
+
     void BuildItemDB()
     {
         items = new List<Item>()
@@ -30,54 +35,54 @@ public class ItemDB : MonoBehaviour
             {
                 {"Poder", 12 },
                 {"Critico", 24}
-            }),
+            }, 10, "Arma"),
             new Item(2, "Espada de fuego", "Daño de fuego a enemigos",
             new Dictionary<string, int>
             {
                 {"Poder", 13 },
                 {"Critico", 28 }
-            }, 20),
+            }, 20,"Arma"),
             new Item(3, "Espada de diamante", "Espada con mas daño",
             new Dictionary<string, int>
             {
                 {"Poder", 20 },
                 {"Critico", 40 }
-            }, 150),
+            }, 150, "Arma"),
             new Item(4, "Espada de hielo", "Efecto de congelacion",
             new Dictionary<string, int>
             {
                 {"Poder", 16 },
                 {"Critico", 32 }
-            }, 40),
+            }, 40, "Arma"),
             new Item(5, "Manzanas", "Restaura un poco de salud",
             new Dictionary<string, int>
             {
                 {"Curacion", 10 },
-            }, 10),
+            }, 10, "Salud"),
             new Item(6, "Pocion", "Restaura 25 de vida",
             new Dictionary<string, int>
             {
                 {"Curacion", 25 },
-            }, 25),
+            }, 25, "Salud"),
             new Item(7, "Elixir", "Cura la mitad de vida",
             new Dictionary<string, int>
             {
                 {"Curacion", 50 },
-            }, 45),
+            }, 45, "Salud"),
             new Item(8, "Armadura Alpha", "Proteccion basica para la aventura",
             new Dictionary<string, int>
             {
                 {"Defensa", 10 },
-            }, 20),
+            }, 20, "Armadura"),
             new Item(7, "Armadura Beta", "Eficientes protecciones",
             new Dictionary<string, int>
             {
                 {"Defensa ", 25 },
-            }, 45),
+            }, 45, "Armadura"),
             new Item(7, "Armadura Omega", "La mejor armadura del juego",
             new Dictionary<string, int>
             {
-                {"Defensa", 55 },
+                {"Defensa", 55 ,"Armadura"},
             }, 145)
         };
     }
