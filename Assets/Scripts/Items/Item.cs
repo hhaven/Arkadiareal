03ce6c3244af652add70eaef1797bb6d620408e5
RafaMelara98 <00182516@uca.edu.sv>
2020-07-11 02:37:32 -0600

    Add item DB

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Item
+{
+	public int id;
+	public string title;
+	public string decription;
+	public Dictionary<string, int> stats = new Dictionary<string, int>();
+	public int price;
+
+	public Item(int id, string title, string description,  Dictionary<string, int> stats, int price)
+	{
+		this.id = id;
+		this.title = title;
+		this.decription = decription;
+		this.stats = stats;
+		this.price = price;
+	}
+
+	public Item(Item item)
+	{
+		this.id = item.id;
+		this.title = item.title;
+		this.decription = item.decription;
+		this.stats = item.stats;
+		this.price = item.price;
+	}
+
+}
---
+++ Assets/Scripts/Items/ItemDB.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDB : MonoBehaviour
+{
+    public List<Item> items = new List<Item>();
+
+    void Awake()
+    {
+        BuildItemDB();
+    }
+
+    public ItemDB GetItem(int id)
+    {
+        return items.Find(item => item.id == id);
+    }
+
+    public ItemDB GetItem(string title)
+    {
+        return items.Find(item => item.title == title);
+    }
+
+    void BuildItemDB()
+    {
+        items = new List<Item>()
+        {
+            new Item(1, "Espada", "Espada de con filo",
+            new Dictionary<string, int>
+            {
+                {"Poder", 12 },
+                {"Critico", 24}
+            }),
+            new Item(2, "Espada de fuego", "Daño de fuego a enemigos",
+            new Dictionary<string, int>
+            {
+                {"Poder", 13 },
+                {"Critico", 28 }
+            }, 20),
+            new Item(3, "Espada de diamante", "Espada con mas daño",
+            new Dictionary<string, int>
+            {
+                {"Poder", 20 },
+                {"Critico", 40 }
+            }, 150),
+            new Item(4, "Espada de hielo", "Efecto de congelacion",
+            new Dictionary<string, int>
+            {
+                {"Poder", 16 },
+                {"Critico", 32 }
+            }, 40),
+            new Item(5, "Manzanas", "Restaura un poco de salud",
+            new Dictionary<string, int>
+            {
+                {"Curacion", 10 },
+            }, 10),
+            new Item(6, "Pocion", "Restaura 25 de vida",
+            new Dictionary<string, int>
+            {
+                {"Curacion", 25 },
+            }, 25),
+            new Item(7, "Elixir", "Cura la mitad de vida",
+            new Dictionary<string, int>
+            {
+                {"Curacion", 50 },
+            }, 45),
+            new Item(8, "Armadura Alpha", "Proteccion basica para la aventura",
+            new Dictionary<string, int>
+            {
+                {"Defensa", 10 },
+            }, 20),
+            new Item(7, "Armadura Beta", "Eficientes protecciones",
+            new Dictionary<string, int>
+            {
+                {"Defensa ", 25 },
+            }, 45),
+            new Item(7, "Armadura Omega", "La mejor armadura del juego",
+            new Dictionary<string, int>
+            {
+                {"Defensa", 55 },
+            }, 145)
+        };
+    }
+
+}
